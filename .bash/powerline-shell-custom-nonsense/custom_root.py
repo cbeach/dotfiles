59aa258ae3a0437350b7cc1498e0944275360e18
casebeac <casebeac@amazon.com>
2021-07-26 10:04:03 -0700

    update powerline with emojis

---
+++ .bash/powerline-shell-custom-nonsense/custom_root.py
@@ -0,0 +1,24 @@
+from powerline_shell.utils import BasicSegment
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        powerline = self.powerline
+        root_indicators = {
+            'bash': ' 🤩️ ',
+            'tcsh': ' %# ',
+            'zsh': ' %# ',
+            'bare': ' $ ',
+        }
+        bg = powerline.theme.CMD_PASSED_BG
+        fg = powerline.theme.CMD_PASSED_FG
+        if powerline.args.prev_error != 0:
+            root_indicators = {
+                'bash': ' 🤬️ ',
+                'tcsh': ' %# ',
+                'zsh': ' %# ',
+                'bare': ' $ ',
+            }
+            fg = powerline.theme.CMD_FAILED_FG
+            bg = powerline.theme.CMD_FAILED_BG
+        powerline.append(root_indicators[powerline.args.shell], fg, bg, sanitize=False)
---
+++ .bash/powerline-shell-custom-nonsense/custom_theme.py
@@ -0,0 +1,8 @@
+from powerline_shell.themes.default import DefaultColor
+
+class Color(DefaultColor):
+    CMD_PASSED_BG = 236
+    CMD_PASSED_FG = 15
+    CMD_FAILED_BG = 236
+    CMD_FAILED_FG = 15
+
