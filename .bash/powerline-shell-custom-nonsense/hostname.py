c1afc5f0b0cd657ebcf7eab6f458f36ffaf05cc7
casebeac <casebeac@amazon.com>
2021-07-30 11:44:28 -0700

    more powerline upgrades, nuke awesomewm

---
+++ .bash/powerline-shell-custom-nonsense/hostname.py
@@ -0,0 +1,32 @@
+from powerline_shell.utils import BasicSegment
+from powerline_shell.color_compliment import stringToHashToColorAndOpposite
+from powerline_shell.colortrans import rgb2short
+from socket import gethostname
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        powerline = self.powerline
+        hostname = gethostname().split(".")[0]
+        if hostname == "ud3bb6c3dba1656":
+            hostname = "wheatly"
+        elif hostname == "dev-dsk-casebeac-2a-a88e73ff":
+            hostname = "servitor1"
+        elif hostname == "dev-dsk-casebeac-2a-446b42f6":
+            hostname = "servitor2"
+
+        if powerline.segment_conf("hostname", "colorize"):
+            FG, BG = stringToHashToColorAndOpposite(hostname)
+            FG, BG = (rgb2short(*color) for color in [FG, BG])
+            host_prompt = " %s " % hostname.split(".")[0]
+            powerline.append(host_prompt, FG, BG)
+        else:
+            if powerline.args.shell == "bash":
+                host_prompt = r" \h "
+            elif powerline.args.shell == "zsh":
+                host_prompt = " %m "
+            else:
+                host_prompt = " %s " % hostname
+            powerline.append(host_prompt,
+                             powerline.theme.HOSTNAME_FG,
+                             powerline.theme.HOSTNAME_BG)
